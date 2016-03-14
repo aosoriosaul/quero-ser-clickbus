314014849647a6968fdad76920ec22896307d6cd
aosoriosaul <aosoriosaul@hotmail.com>
2016-03-14 11:16:30 -0600

    Create cash.php
    Software creado en PHP por Alfonso Antonio Osorio Cid, no pude realizarlo con el framework Symfony2 ya que veo que cambio la manera de instalar y configurar y estoy realizando el analisis de este para poder llevarlo a cabo.

---
+++ testes/cash-machine/cash.php
@@ -0,0 +1,14 @@
+<!--Examen Cash machine-->
+<html>
+<head>
+	<title>Examen Cash machine</title>
+</head>
+<body>
+	<H1>RETIRO CAJERO AUTOMATICO</H1>
+	INTRODUZCA IMPORTE
+	<FORM ACTION="procesa.php" METHOD="POST">
+	<INPUT TYPE="number" NAME="efectivo" id="efectivo"><BR><BR>
+	<INPUT TYPE="submit" VALUE="Enviar">
+	</FORM>
+</body>
+</html>
